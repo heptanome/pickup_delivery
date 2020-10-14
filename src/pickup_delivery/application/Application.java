3874a3f26915e868a69918eac343968a1f6ab32e
Paul Bodelot <49484206+paulbodelot@users.noreply.github.com>
2020-10-14 11:22:35 +0200

    Premiere version de notre magnifique logiciel

---
+++ src/pickup_delivery/application/Application.java
@@ -0,0 +1,9 @@
+package pickup_delivery.application;
+
+public class Application {
+
+	public static void main (String ... args) {
+		System.out.println("Bienvenue sur Pickup and Delivery");
+	}
+	
+}
