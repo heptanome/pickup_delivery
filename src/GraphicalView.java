04d020cb1238581d254cb3c3d4a337498bfdf0ad
Camille <camille.peltier04@gmail.com>
2020-10-19 13:12:54 +0200

    visualisation of intersections in progress

--- src/GraphicalView.java
+++ src/GraphicalView.java
@@ -1,14 +1,20 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.LinkedList;
 import java.util.List;
 
 public class GraphicalView  extends JPanel{
-    List<Intersection> instersections;
+    List<Intersection> intersections;
     List<Segment> segments;
+
+    List<GraphicalPoint> graphicalPoints;
     
     public GraphicalView(Map loadedMap){
         setLayout(null);
         setBounds(0,0,800,800);
+        intersections = loadedMap.getInstersections();
+        segments = loadedMap.getSegments();
+        graphicalPoints = new LinkedList<GraphicalPoint>();
     }
 
     public void paint(Graphics g){
@@ -18,6 +24,18 @@ public class GraphicalView  extends JPanel{
         //Test
         g.setColor(Color.orange);
         g.fillOval(20,20,10,10);
+
+        g.setColor(Color.black);
+        /*
+        for (Intersection i : intersections) {
+            GraphicalPoint gp = new GraphicalPoint(i.getLatitude(), i.getLongitude());
+            graphicalPoints.add(gp);
+            g.fillOval(gp.getXPixel(), gp.getYPixel(), 10, 10);
+            System.out.println(gp.getXPixel());
+            System.out.println(gp.getYPixel());
+        }
+        System.out.println(graphicalPoints.size());*/
+        g.fillOval(400, 400, 10, 10);
     }
 
     
