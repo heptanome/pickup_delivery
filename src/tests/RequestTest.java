5fc91be7262da0341a590a6575bbdf6b162c91e5
Guillaume <guillaume@kessibi.fr>
2020-10-21 00:42:16 +0200

    CI: added Junit classes necessary for building and running tests

--- src/tests/RequestTest.java
+++ src/tests/RequestTest.java
@@ -1,10 +1,8 @@
 package tests;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
