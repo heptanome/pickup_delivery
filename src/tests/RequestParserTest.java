27f2f35f89a5254d6e2e46a3cc4f29f9c33a6950
INSArthur <49277717+INSArthur@users.noreply.github.com>
2020-10-20 22:50:31 +0200

    RequestParserTest

--- src/tests/RequestParserTest.java
+++ src/tests/RequestParserTest.java
@@ -6,10 +6,19 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import model.RequestParser;
+import model.SetOfRequests;
+
 class RequestParserTest {
+	
+	public RequestParser requestParser;
+	public static final String FILE_PATH = "./XML_data/requestsLarge9.xml";
+	public static final int NB_REQUEST = 9;
+	public static final String ID_DEPOT = "25327124";
 
 	@BeforeEach
 	void setUp() throws Exception {
+		requestParser = new RequestParser(FILE_PATH);
 	}
 
 	@AfterEach
@@ -17,13 +26,10 @@ class RequestParserTest {
 	}
 
 	@Test
-	void testRequestParser() {
-		fail("Not yet implemented");
-	}
-
-	@Test
 	void testLoadRequests() {
-		fail("Not yet implemented");
+		SetOfRequests setOfRequest = requestParser.loadRequests();
+		assertEquals(setOfRequest.getDepot(),ID_DEPOT);
+		assertEquals(setOfRequest.getRequests().size(),NB_REQUEST);
 	}
 
 }
