df3e51007c0d97fcd016beec1a475fd50d29ac97
Phanie Buonomo <fanny.buonomo@insa-lyon.fr>
2020-10-21 05:00:47 +0200

    added COmputeTour method

--- src/model/Tour.java
+++ src/model/Tour.java
@@ -2,6 +2,13 @@ package model;
 
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Map;
+
+import tsp.*;
+
 
 public class Tour {
 	public CityMap map;
@@ -36,5 +43,66 @@ public class Tour {
 		this.setOfRequests = rp.loadRequests();
 		// signal the observers the set of requests has changed
 		support.firePropertyChange("updateRequests", oldReq, this.setOfRequests);
+	}
+	
+	private CompleteGraph mapToCompleteGraph() {
+		int vertices = map.getNbVertices();
+	    Map<String,Integer> numberToIdMap =  map.getNumberIdMap();
+	    List<Segment> segments= map.getSegments();
+	    String[] requestNodes = setOfRequests.getRequestNodes();
+	    CompleteGraph g = new CompleteGraph(vertices,numberToIdMap,segments,requestNodes);
+	    return g;
+	}
+	
+	public List<Segment> computeTour(){
+		TSP tsp = new TSP1();
+		CompleteGraph g = mapToCompleteGraph();
+		List<Segment> path = new LinkedList<Segment>();
+		long startTime = System.currentTimeMillis();
+		
+		tsp.searchSolution(20000, g);
+		System.out.print("Solution of cost "+tsp.getSolutionCost()+" found in "
+				+(System.currentTimeMillis() - startTime)+"ms : ");
+		
+		
+		int[] solutionInt = new int[setOfRequests.getRequestNodes().length+1];
+		String[] solutionString = new String[setOfRequests.getRequestNodes().length+1];
+		
+		for (int i=0; i<setOfRequests.getRequestNodes().length; i++)
+			solutionInt[i]=tsp.getSolution(i);
+		solutionInt[solutionInt.length-1] = 0;
+		
+		Map<Integer,String> nodeNames = g.getNodeNames();
+		for(int i=0; i < solutionInt.length; i++) {
+			solutionString[i] = nodeNames.get(solutionInt[i]);
+		}
+
+		System.out.println("Depot : "+solutionString[0]);
+		System.out.print("Next intersection id to visit : ");
+		List<Integer> intermediateNodes = new LinkedList<Integer>();
+		for(int indexSol = 0; indexSol < solutionString.length-1; indexSol++) {
+			int idOrigine = map.getIntFromNumberMap(solutionString[indexSol]);
+			int idDepart = map.getIntFromNumberMap(solutionString[indexSol+1]);
+			int[] precedence = g.getPrecedenceOfANode(idOrigine);
+			for (int i=idDepart; i!= idOrigine; i=precedence[i]) {
+				intermediateNodes.add(i);
+			}
+
+			ListIterator<Integer> iterator = intermediateNodes.listIterator(intermediateNodes.size()); 
+			String currentNodeNumber = solutionString[0];
+			String previousNodeNumber = solutionString[0];
+			while(iterator.hasPrevious()){
+				int previousNodeId = iterator.previous();
+				previousNodeNumber = map.getStringFromIdMap(previousNodeId);
+				path.add(map.getSegmentFromPoints(currentNodeNumber, previousNodeNumber));
+				currentNodeNumber = previousNodeNumber;
+				System.out.print(previousNodeNumber+" | ");
+			}
+			intermediateNodes.clear();
+		}
+		System.out.println();
+		System.out.println(path);
+		return path;
+		
 	}
 }
