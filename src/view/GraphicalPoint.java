cc59dbbb9464e03933765f37b6cc3443a909292a
Camille <camille.peltier04@gmail.com>
2020-11-07 19:23:26 +0100

    javadoc comments in GraphicalPoint and GraphicalSegment + modified GraphicalSegment attributes to b be more consistent

--- src/view/GraphicalPoint.java
+++ src/view/GraphicalPoint.java
@@ -4,35 +4,49 @@ import java.lang.Math;
 import java.awt.*;
 import model.Intersection;
 
+/**
+ * Graphical representation of an Intersection
+ * Color code: white: random intersection blue : pickup point magenta : delivery
+ * point yellow : departure
+ */
 public class GraphicalPoint {
 
-	private int size = 8;
-	private int xPixel;
-	private int yPixel;
+	private int size = 8; //diameter of the point
+	private int xPixel; 
+	private int yPixel; 
 	private Intersection point;
-	private boolean isSpecial;
-	private Color color;
-	private final int mapSize = 800;
-	private final int mapMargin = 10;
-	/*
-	 * Color code: white: random intersection blue : pickup point magenta : delivery
-	 * point yellow : departure
+	private boolean isSpecial = false; 
+	private Color color = Color.white;
+	private final int MAP_SIZE = 800;
+	private final int MAP_MARGIN = 10;
+
+	/**
+	 * Contructor
+	 * @param i the intersection that will be represented by the GraphicalPoint
+	 * @param minLat the minimum latitude of all the existing intersections 
+	 * @param minLongi he minimum longitude of all the existing intersections
+	 * @param maxLat he maximum latitude of all the existing intersections
+	 * @param maxLongi the maximum longitude of all the existing intersections
 	 */
 	public GraphicalPoint(Intersection i, float minLat, float minLongi, float maxLat, float maxLongi) {
 		float xRange = maxLat - minLat;
 		float yRange = maxLongi - minLongi;
 
-		float x = (float) ((i.getLatitude() - minLat) * mapSize / xRange );
-		float y = (float) ((i.getLongitude() - minLongi) * mapSize / yRange );
+		float x = (float) ((i.getLatitude() - minLat) *  MAP_SIZE / xRange );
+		float y = (float) ((i.getLongitude() - minLongi) * MAP_SIZE / yRange );
 
-		xPixel = Math.round(x) +mapMargin - size/2;
-		yPixel = Math.round(y) +mapMargin - size/2;
+		xPixel = Math.round(x) + MAP_MARGIN - size/2;
+		yPixel = Math.round(y) + MAP_MARGIN - size/2;
 		point = i;
-		color = Color.white;
-		isSpecial  = false;
 		
 	}
 
+	/**
+	 * 
+	 * @param x horizontal coordinate of the point clicked
+	 * @param y vertical coordinate of the point clicked
+	 * @return true if the (x,y) belongs to the circle of center(xPixel,yPixel) and of radius size/2
+	 */
 	public boolean isClicked(int x, int y){
 		if(Math.sqrt( (x-(xPixel+size/2))*(x-(xPixel+size/2)) + (y-(yPixel+size/2))*(y-(yPixel+size/2)) )<= size/2){
 			return true;
@@ -41,22 +55,41 @@ public class GraphicalPoint {
 		}
 	}
 
+	/**
+	 * Getter for xPixel attribute
+	 * @return xPixel
+	 */
 	public int getXPixel() {
 		return xPixel;
 	}
 
+	/**
+	 * Getter for yPixel attribute
+	 * @return yPixel
+	 */
 	public int getYPixel() {
 		return yPixel;
 	}
-
+	/**
+	 * Getter for point attribute
+	 * @return point
+	 */
 	public Intersection getPoint() {
 		return point;
 	}
 
+	/**
+	 * Getter for color attribute
+	 * @return color
+	 */
 	public Color getColor() {
 		return color;
 	}
 
+	/**
+	 * Setter for color attribute
+	 * @param c the new color
+	 */
 	public void setColor(Color c) {
 		color = c;
 		if(c == Color.white){
@@ -67,18 +100,34 @@ public class GraphicalPoint {
 		
 	}
 
-	public void setIsSpecial(boolean b){
-		isSpecial = b;
-	}
-
+	/**
+	 * Getter for isSpecial attribute
+	 * @return isSpecial
+	 */
 	public boolean getIsSpecial(){
 		return isSpecial;
 	}
 
+	/**
+	 * Setter of isSpecial attribute
+	 * @param b the new value for isSpecial
+	 */
+	public void setIsSpecial(boolean b){
+		isSpecial = b;
+	}
+
+	/**
+	 * Getter for size attribute
+	 * @return size, the diameter of the point
+	 */
 	public int getSize() {
 		return size;
 	}
 
+	/**
+	 * Setter of size attribute
+	 * @param i the new size (diameter)
+	 */	
 	public void setSize(int i) {
 		//Recenter the point
 		xPixel += (size/2 - i/2);
--- src/view/GraphicalSegment.java
+++ src/view/GraphicalSegment.java
@@ -2,68 +2,114 @@ package view;
 
 import java.awt.*;
 
+
+/**
+ * Graphical representation of a Segment
+ * Color code: white: random segment ; red : segment used by the tour
+ */
 public class GraphicalSegment {
+	private GraphicalPoint origin;
+	private GraphicalPoint destination;
 	private int xOriginPixel;
 	private int yOriginPixel;
 	private int xDestPixel;
 	private int yDestPixel;
-	private Color color;
-	private String idOrigin;
-	private String idDest;
-	private int onPath;
-	/*
-	 * Color code: white: random segment blue : segment used by the tour
+	private Color color = Color.white;
+	private int onPath = 0;
+
+	/**
+	 * Constructor
+	 * @param o the GraphicalPoint representing the origin Intersection
+	 * @param d the GraphicalPoint representing the destination Intersection
+	 * 
 	 */
-
-	public GraphicalSegment(String origin, String dest, int xo, int yo, int xd, int yd) {
+	public GraphicalSegment(GraphicalPoint o, GraphicalPoint d) {
+		origin = o;
+		destination = d;
 		// +4 so that the line starts from the center of the point
-		idOrigin = origin;
-		idDest = dest;
-		xOriginPixel = xo + 4;
-		yOriginPixel = yo + 4;
-		xDestPixel = xd + 4;
-		yDestPixel = yd + 4;
-		color = Color.white;
-		onPath = 0;
+		xOriginPixel = o.getXPixel() + 4;
+		yOriginPixel = o.getYPixel() + 4;
+		xDestPixel = d.getXPixel() + 4;
+		yDestPixel = d.getYPixel() + 4;
 
 	}
-	
+
+	/**
+	 * Getter for the address number of the origin intersection
+	 * @return the address number of the intersection represented by the origin attribute
+	 */	
 	public String getOrigin() {
-		return idOrigin;
+		return origin.getPoint().getNumber();
 	}
 	
+	/**
+	 * Getter for onPath attribute
+	 * @return onPath
+	 */		
 	public int getOnPath() {
 		return onPath;
 	}
-	
+
+	/**
+	 * Getter for the address number of the destination intersection
+	 * @return the address number of the intersection represented by the destination attribute
+	 */		
 	public String getDestination() {
-		return idDest;
+		return destination.getPoint().getNumber();
 	}
 
+	/**
+	 * Getter for XOriginPixel attribute
+	 * @return XOriginPixel
+	 */	
 	public int getXOriginPixel() {
 		return xOriginPixel;
 	}
 
+	/**
+	 * Getter for YOriginPixel attribute
+	 * @return YOriginPixel
+	 */	
 	public int getYOriginPixel() {
 		return yOriginPixel;
 	}
 
+	/**
+	 * Getter for XDestPixel attribute
+	 * @return XDestPixel
+	 */	
 	public int getXDestPixel() {
 		return xDestPixel;
 	}
 
+	/**
+	 * Getter for YDestPixel attribute
+	 * @return YDestPixel
+	 */	
 	public int geYDestPixel() {
 		return yDestPixel;
 	}
 
+	/**
+	 * Getter for color attribute
+	 * @return color
+	 */	
 	public Color getColor() {
 		return color;
 	}
 
+	/**
+	 * Setter for color attribute
+	 * @param c the new color
+	 */	
 	public void setColor(Color c) {
 		color = c;
 	}
-	
+
+	/**
+	 * Setter for onPath attribute
+	 * @param newOnPath the new onPath
+	 */	
 	public void setOnPath(int newOnPath) {
 		onPath = newOnPath;
 	}
