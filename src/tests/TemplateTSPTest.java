2e34cddba096d16f9b6f634f1ad16a0bbb38fb2c
Luise <lu.zie@t-online.de>
2020-11-09 21:21:29 +0100

    added a basic test for TSP

---
+++ src/tests/TemplateTSPTest.java
@@ -0,0 +1,34 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+abstract class TemplateTSPTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testSearchSolution() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetSolution() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetSolutionCost() {
+		fail("Not yet implemented");
+	}
+
+}
